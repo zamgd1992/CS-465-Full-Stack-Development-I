bb782ebf239a6748afc9dcd70ce5cb77218ec2f3
zamgd1992 <zacmcbride90@gmail.com>
2023-09-11 11:53:03 -0600

    Convert travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 const travel = (req, res) => {
     console.log('Inside app_server, controllers, travel.js index function.');
     pageTitle = process.env.npm_package_description + ' - Travel'
-    res.render('travel', { title: pageTitle })
+    res.render('travel', { title: pageTitle, trips })
 };
 module.exports = {
     travel
